3f96ca9b77cb315fc54aafa0f1ec62aac826c3a8
Liya <liyaaugustinek@gmail.com>
2021-09-24 22:35:54 +0530

    removed myapp

--- myapp/serializers.py
+++
@@ -1,6 +0,0 @@
-from rest_framework import serializers
-from myapp.models import *
-class Userserializer(serializers.ModelSerializer):
-    class Meta:
-        model=AjaxUdetails
-        fields=('id','name','contact','place')
