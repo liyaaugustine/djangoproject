f71fb5c5de072fcc77025676abb88f68aa11cfbe
Liya <liyaaugustinek@gmail.com>
2021-09-24 10:53:41 +0530

    edited settings

--- myproject/settings.py
+++ myproject/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    'rest_framework',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -40,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'myapp',
     'second',
-    'rest_framework',
+    
     
 ]
 
